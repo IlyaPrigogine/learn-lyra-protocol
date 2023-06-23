d157ab8baf4f4a6b8d89f226a42b7c60a9cd627a
IlyaPrigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2023-06-23 14:50:55 +0800

    Update 2_AdjustingPositions.ts

--- test/contracts/OptionToken/2_AdjustingPositions.ts
+++ test/contracts/OptionToken/2_AdjustingPositions.ts
@@ -5,7 +5,9 @@ import { allTradesFixture } from '../../utils/fixture';
 import { expect, hre } from '../../utils/testSetup';
 
 describe('OptionToken - AdjustingPositions', async () => {
-  beforeEach(allTradesFixture);
+  beforeEach(async() => {
+    await allTradesFixture();
+  });
   it('gets all positions as expected', async () => {
     expect(await hre.f.c.optionToken.balanceOf(hre.f.deployer.address)).eq(5);
     let position: PositionWithOwnerStruct;
@@ -31,5 +33,20 @@ describe('OptionToken - AdjustingPositions', async () => {
       expect(await hre.f.c.optionToken.canLiquidate(position, 0, 0, 0)).to.be.false;
     }
     expect((await hre.f.c.optionToken.getOwnerPositions(hre.f.deployer.address)).length).eq(0);
+  });
+
+  it.only('test001 => ot', async () => {
+    const ot = await hre.f.c.optionToken;
+    const deployer = hre.f.deployer;
+    let position: PositionWithOwnerStruct;
+    for (const optionType of ALL_TYPES) {
+      position = await ot.getPositionWithOwner(hre.f.positionIds[optionType]);
+      expect(position.owner).eq(deployer.address);
+      expect(position.strikeId).eq(hre.f.strike.strikeId);
+      expect(position.optionType).eq(optionType);
+      expect(position.amount).eq(DEFAULT_OPTIONS[optionType].amount);
+      expect(position.state).eq(PositionState.ACTIVE);
+      expect(position.collateral).to.eq((DEFAULT_OPTIONS[optionType] as any).setCollateralTo || 0);
+    }
   });
 });
